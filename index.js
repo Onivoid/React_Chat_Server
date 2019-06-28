4eb148dcdee7c60a2058a0be65bd19d862712278
Logan Wilquin <lwilquin@datasolution.fr>
2019-06-28 17:13:01 +0200

    emit event 'newUser'

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const port = process.env.PORT || 3001;
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.emit('ServerON')
+  socket.emit('ServerON');
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+
+  socket.on('newUser', () => {
+    io.emit('newUser');
+  })
 
   socket.on('newMessage', ({username, message})=>{
     io.emit('newMessage', {username, message});
